eb30c6b97f7096e77431263d8d6f221a73d60e9a
maria shalneva <32167608+mariashalneva@users.noreply.github.com>
2020-01-20 07:24:56 -0500

    music for oceans

--- Assets/Scripts/CubeManager.cs
+++ Assets/Scripts/CubeManager.cs
@@ -9,18 +9,27 @@ public enum State { one, two, three, four }
 public class CubeManager : MonoBehaviour
 {
     public GameObject[] cubes;
+    public AudioClip[] audio;
 
     public Slider sliderVal;
+    public AudioSource source;
+
+    private void Awake()
+    {
+        source = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>();
+        sliderVal.value = 0;
+    }
+
     public void OnValueChanged()
     {
         if (sliderVal.value <= 0)
-            ChangeView(0);
-        else if (sliderVal.value > 0.2 && sliderVal.value <= 0.3)
             ChangeView(1);
-        else if (sliderVal.value > 0.4 && sliderVal.value <= 0.7)
+        else if (sliderVal.value > 0.2 && sliderVal.value <= 0.3)
             ChangeView(2);
-        else if (sliderVal.value > 0.75 && sliderVal.value <= 1)
+        else if (sliderVal.value > 0.4 && sliderVal.value <= 0.7)
             ChangeView(3);
+        else if (sliderVal.value > 0.75 && sliderVal.value <= 1)
+            ChangeView(4);
 
     }
 
@@ -32,27 +41,33 @@ public class CubeManager : MonoBehaviour
                 {
                     cubes.ToList().ForEach(x => x.SetActive(false));
                     cubes[0].gameObject.SetActive(true);
+                    source.clip = audio[0];
                 }
                 break;
             case 2:
                 {
                     cubes.ToList().ForEach(x => x.SetActive(false));
                     cubes[1].gameObject.SetActive(true);
+                    source.clip = audio[1];
                 }
                 break;
             case 3:
                 {
                     cubes.ToList().ForEach(x => x.SetActive(false));
                     cubes[2].gameObject.SetActive(true);
+                    source.clip = audio[2];
                 }
                 break;
             case 4:
                 {
                     cubes.ToList().ForEach(x => x.SetActive(false));
                     cubes[3].gameObject.SetActive(true);
+                    source.clip = audio[3];
                 }
                 break;
         }
+
+        source.Play();
     }
 
 }
